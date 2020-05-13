8c3e321376810a7d29070f00be0d817039d4fc8c
riko <rikochyou@outlook.com>
2020-05-13 17:54:23 +0800

    优化首屏加载速度

---
+++ vue.config.js
@@ -0,0 +1,51 @@
+const CompressionPlugin = require("compression-webpack-plugin");
+module.exports = {
+  chainWebpack: config => {
+    /* 添加分析工具*/
+    if (process.env.NODE_ENV === "production") {
+      if (process.env.npm_config_report) {
+        config
+          .plugin("webpack-bundle-analyzer")
+          .use(require("webpack-bundle-analyzer").BundleAnalyzerPlugin)
+          .end();
+        config.plugins.delete("prefetch");
+      }
+    }
+
+    config.optimization.splitChunks({
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\/]node_modules[\/]/, // 指定是node_modules下的第三方包
+          name: "chunk-vendors",
+          chunks: "all",
+          priority: -10 // 抽取优先级
+        },
+        // 抽离自定义工具库
+        utilCommon: {
+          name: "chunk-common",
+          minSize: 1024, // 将引用模块分离成新代码文件的最小体积
+          minChunks: 2, // 表示将引用模块如不同文件引用了多少次，才能分离生成新chunk
+          priority: -20
+        }
+      }
+    });
+  };
+
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "production") {
+      // 为生产环境修改配置...
+      config.mode = "production";
+      return {
+        plugins: [
+          new CompressionPlugin({
+            test: /\.js$|\.html$|\.css/, //匹配文件名
+            threshold: 10240, //对超过10k的数据进行压缩
+            deleteOriginalAssets: false //是否删除原文件
+          })
+        ]
+      };
+    }
+  };
+
+}
